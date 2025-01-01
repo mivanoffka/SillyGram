994020db139da5084f5d457f9715b6e755ce5b92
mivanoffka <sharariki@gmail.com>
2025-01-01 13:52:55 +0300

    Fixed event.py absence in last commit

---
+++ sillygram/events/event.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict, Tuple
+
+if TYPE_CHECKING:
+    from .. import SillyUser
+
+class SillyEvent:
+    _user: SillyUser
+    _args: Tuple
+    _kwargs: Dict
+    
+    @property
+    def user(self) -> SillyUser:
+        return self._user
+    
+    @property
+    def args(self) -> Tuple:
+        return self._args
+    
+    @property
+    def kwargs(self) -> Dict:
+        return self._kwargs
+    
+    def __init__(self, user: SillyUser, *args, **kwargs):
+        self._user = user
+        self._args = args
+        self._kwargs = kwargs
