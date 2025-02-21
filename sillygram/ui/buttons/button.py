802f09115f7b5452bad7483f7cc3b891cbcf7a17
mivanoffka <sharariki@gmail.com>
2025-02-21 22:26:58 +0300

    SillyButton objects can now be instantiated

--- sillygram/ui/buttons/button.py
+++ sillygram/ui/buttons/button.py
@@ -1,4 +1,5 @@
 from typing import Any
+from aiogram.types import InlineKeyboardButton
 
 from ...text import SillyText
 
@@ -10,8 +11,8 @@ class SillyButton:
     def text(self) -> SillyText:
         return self._text
 
-    def aiogramify(self, language_code: str) -> Any:
-        raise NotImplementedError()
+    def aiogramify(self, language_code: str) -> InlineKeyboardButton:
+        return InlineKeyboardButton(text=self._text.localize(language_code))
 
     def __init__(self, text: SillyText):
         self._text = text
